99524ba74a5040bfb1eac9f8e90274166f595ac7
Your Name <mr.trungtv@gmail.com>
2012-02-08 23:20:42 +0000

    First commit

---
+++ src/Cloud/Sync/Sync.java
@@ -0,0 +1,13 @@
+package Cloud.Sync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sync extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
